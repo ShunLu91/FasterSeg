f2e5666dfd230e93649fb53a553ef4fb445e67e8
lushun_mac <470651748@qq.com>
2020-07-27 22:50:55 +0800

    data_dir

--- search/run.sh
+++ search/run.sh
@@ -3,7 +3,7 @@ if [ ! -d logdir  ];then
 fi
 
 #CUDA_VISIBLE_DEVICES=6 python train_search.py
-CUDA_VISIBLE_DEVICES=6 nohup python -u train_search.py  > logdir/fs0.log  2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python -u train_search.py  > logdir/fs0.log  2>&1 &
 
 tail -f logdir/fs0.log
 

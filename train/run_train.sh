45caac931863d3e36afd4aa75e35adb64413a68c
lushun_mac <470651748@qq.com>
2020-08-04 17:31:43 +0800

    fs0_f

--- train/run_train.sh
+++ train/run_train.sh
@@ -10,7 +10,7 @@
 #CUDA_VISIBLE_DEVICES=0 nohup python -u train_search.py  > ../logdir/fs0_s.log  2>&1 &
 
 #******************************* full train *******************************#
-CUDA_VISIBLE_DEVICES=0 nohup python -u train.py  > ../logdir/fs0_f.log  2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup python -u train.py  > ../logdir/fs0_f.log  2>&1 &
 
 
 tail -f ../logdir/fs0_f.log
